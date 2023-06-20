48b8936f84e5db52e462a8540252d2eb84af25c0
artemiy-228 <artemsch17@gmail.com>
2023-06-20 22:08:32 +0500

    Edition v 1.1

--- Script.sh
+++ Script.sh
@@ -1,11 +1,19 @@
-echo -n "Input username: "
+path=$(dirname "$(realpath "$0")")
+echo -n "Do you want to save this file's i folder in this directory? (yes, no)"
+read t
+if [ t -eq "no"] || [ t -eq "n"] || || [ t -eq "N"] || || [ t -eq "No"]; then
+    echo -n "Input your path to file(firt and last without /): "
+    read path
+fi
 read username
 
-mkdir -p /home/$username/Desktop/mp4_videos/
+
+
+mkdir -p /path/mp4_videos/
 
 for file in /home/$username/Desktop/video/*.vob; do
     filename=$(basename "$file")
     filename="${filename%.*}"
 
-    ffmpeg -i "$file" "/home/$username/Desktop/mp4_videos/${filename}.mp4"
+    ffmpeg -i "$file" "/path/mp4_videos/${filename}.mp4"
 done
