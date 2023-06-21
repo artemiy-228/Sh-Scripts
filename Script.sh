7e64705d475bfc89f7c1603e96f3369cf9c85688
ArtemKa <artemsch17@gmail.com>
2023-06-20 22:47:27 -0700

    Converter VOB to mp4

--- Script.sh
+++ Script.sh
@@ -1,28 +1,12 @@
-i_path=$(dirname "$(realpath "$0")")
+#!/bin/bash
 
-echo -n "Do you want to save the new video file in this directory ($i_path/mp4_videos)? (yes, no): "
-read t
-echo ""
-
-if [[ $t = "no" ]]; then
-    echo -n "Input your path to file(first and last without /): "
-    read i_path
-    echo ""
+if [ -n "$1" ]; then
+    output_path=$1
 else
-    echo "Videos will be recorded in the directory: $i_path/mp4_videos"
-    echo ""
+    output_path=$PWD
 fi
 
-echo -n "Input path to file with your videos(first and last without /): "
-read o_path
-echo ""
-
-
-mkdir -p /$i_path/mp4_videos/
-
-for file in /$o_path/*.vob; do
-    filename=$(basename "$file")
-    filename="${filename%.*}"
-
-    ffmpeg -i "$file" "/$i_path/mp4_videos/${filename}.mp4"
+for file in /$output_path/*.vob; do
+    filename=${file::-3}
+    ffmpeg -i "$file" "/$output_path$filenamemp4"
 done
